67a689682924d4d20e1b4cbe8babf00aaa25042a
khaqng <khanhquangnguyennqk@gmail.com>
2024-07-17 12:05:21 +0700

    fix usermapping

--- src/main/java/com/inkmelo/user/UserMappingService.java
+++ src/main/java/com/inkmelo/user/UserMappingService.java
@@ -4,39 +4,46 @@ import java.sql.Date;
 import java.time.LocalDate;
 
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserMappingService {
 
-		
-	public User userCreateBodyDTOToUser(UserCreateBodyDTO userDTO) {
-		return User.builder()
-				.username(userDTO.username())
-				.password(userDTO.password())
-				.email(userDTO.email())
-				.fullname(userDTO.fullname())
-				.role(userDTO.role() == null ? 
-						UserRole.CUSTOMER : userDTO.role())
-				.createdAt(Date.valueOf(LocalDate.now()))
-				.lastChangedBy(SecurityContextHolder.getContext()
-						.getAuthentication().getName())
-				.lastUpdatedTime(Date.valueOf(LocalDate.now()))
-				.status(UserStatus.ACTIVE)
-				.build();
-	}
+    private final PasswordEncoder passwordEncoder;
 
-	public UserAdminResponseDTO userToUserAdminResponseDTO(User user) {
-		return UserAdminResponseDTO.builder()
-				.id(user.getId())
-				.username(user.getUsername())
-				.fullname(user.getFullname())
-				.email(user.getEmail())
-				.role(user.getRole())
-				.createdAt(user.getCreatedAt())
-				.lastChangedBy(user.getLastChangedBy())
-				.lastUpdatedTime(user.getLastUpdatedTime())
-				.status(user.getStatus())
-				.build();
-	}
+    public UserMappingService() {
+        this.passwordEncoder = new BCryptPasswordEncoder();
+    }
+
+    public User userCreateBodyDTOToUser(UserCreateBodyDTO userDTO) {
+        return User.builder()
+                .username(userDTO.username())
+                .password(passwordEncoder.encode(userDTO.password()))
+                .email(userDTO.email())
+                .fullname(userDTO.fullname())
+                .role(userDTO.role() == null ? 
+                        UserRole.CUSTOMER : userDTO.role())
+                .createdAt(Date.valueOf(LocalDate.now()))
+                .lastChangedBy(SecurityContextHolder.getContext()
+                        .getAuthentication().getName())
+                .lastUpdatedTime(Date.valueOf(LocalDate.now()))
+                .status(UserStatus.ACTIVE)
+                .build();
+    }
+
+    public UserAdminResponseDTO userToUserAdminResponseDTO(User user) {
+        return UserAdminResponseDTO.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .fullname(user.getFullname())
+                .email(user.getEmail())
+                .role(user.getRole())
+                .createdAt(user.getCreatedAt())
+                .lastChangedBy(user.getLastChangedBy())
+                .lastUpdatedTime(user.getLastUpdatedTime())
+                .status(user.getStatus())
+                .build();
+    }
 }
